a97ddf7ead531dde3ac99930ec7b4517163d284c
Anil Paswan <33987849+anilpaswan619@users.noreply.github.com>
2021-09-25 09:10:50 +0530

    Updated

--- index.js
+++
@@ -1,8 +0,0 @@
-let arr = ["A", "a", "b","B","c"];
-for(let i=0; i<arr.length; i++) {
-    if(arr[i]===arr){
-        console.log(arr);
-    } else {
-        console.log("No Capital letters")
-    }
-}
